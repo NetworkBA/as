323046761807dc2e123ca76ff1977366a0362ab1
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 01:10:10 -0600

    x

--- resources/js/components/InicioSesion.vue
+++ resources/js/components/InicioSesion.vue
@@ -10,21 +10,18 @@
 
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ ('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email" autofocus>
 
-                                @error('email')
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ ('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right"></label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -51,7 +48,6 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
                                 </button>
 
 
