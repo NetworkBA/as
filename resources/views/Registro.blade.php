7c3c55797aaeaebf4050509e663af7ac9e362aac
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 15:44:18 -0600

    x

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -20,6 +20,8 @@
 		    <div class="row justify-content-center">
 		        <div class="col-md-8">
 		            <div class="card">
+									<div class="card-header"></div>
+
 		                <div class="card-body">
 		                    <form method="POST" action="{{ route('register') }}">
 		                        @csrf
