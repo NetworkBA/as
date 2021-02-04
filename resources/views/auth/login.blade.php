0b43b944f889655f96e64a7575aa42607c400030
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 17:07:35 -0600

    x

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,6 +71,8 @@
     															<button type="submit" class="btn btn-primary">
     																	{{ __('Inicio de Sesión') }}
     															</button>
+																	<hr><a class="btn btn-primary btn-block text-white btn-google btn-user" role="button"><i class="fab fa-google"></i>&nbsp; Login with Google</a><a class="btn btn-primary btn-block text-white btn-facebook btn-user" role="button"><i class="fab fa-facebook-f"></i>&nbsp; Login with Facebook</a>
+                                        <hr>
 
     															@if (Route::has('password.request'))
     																	<a class="btn btn-link" href="{{ route('password.request') }}">
