7c6815050f34d1b91fba8cc119e5cfb47ec55776
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 11:59:50 -0600

    x

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,7 @@
 											@csrf
 
 											<div class="form-group row">
-													<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+													<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electronico') }}</label>
 
 													<div class="col-md-6">
 															<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -39,7 +39,7 @@
 											</div>
 
 											<div class="form-group row">
-													<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+													<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
 													<div class="col-md-6">
 															<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -58,7 +58,7 @@
 																	<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
 																	<label class="form-check-label" for="remember">
-																			{{ __('Remember Me') }}
+																			{{ __('Recordarme') }}
 																	</label>
 															</div>
 													</div>
@@ -72,7 +72,7 @@
 
 															@if (Route::has('password.request'))
 																	<a class="btn btn-link" href="{{ route('password.request') }}">
-																			{{ __('Forgot Your Password?') }}
+																			{{ __('¿Recordad Contraseña?') }}
 																	</a>
 															@endif
 													</div>
