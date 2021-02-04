d2cf47510ae78a2578f3c1c7f09046a8ebad12ba
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 01:45:04 -0600

    x

--- resources/js/components/InicioSesion.vue
+++ resources/js/components/InicioSesion.vue
@@ -7,11 +7,7 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"  required autocomplete="email" autofocus>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                  
                             </div>
                         </div>
 
@@ -21,11 +17,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong></strong>
-                                    </span>
-                                @enderror
+
                             </div>
                         </div>
 
@@ -48,7 +40,7 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" >
+                                    <a class="btn btn-link" href="">
                                         Recuperar Contraseña
                                     </a>
                                 @endif
