1c83902b520972859c3e92a88e4b2d088e4ca233
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-04 23:06:39 -0600

    x

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/laravel-socialite/public/login/google/callback',
+        'redirect' => 'http://public/login/google/callback',
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://localhost/laravel-socialite/public/login/facebook/callback',
+        'redirect' => 'http://public/login/facebook/callback',
     ],
 
 ];
