5bdf86484f4d69912740fa51b2107a4dbc1fc838
NetworkBA <20180366@ulsavictoria.edu.mx>
2021-02-06 16:04:23 -0600

    x

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://mentecuatropuntocero.com/public/login/google/callback',
+        'redirect' => 'http://mentecuatropuntocero.com/login/google/callback',
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'https://mentecuatropuntocero.com/public/login/facebook/callback',
+        'redirect' => 'http://mentecuatropuntocero.com/login/facebook/callback',
     ],
 
 ];
